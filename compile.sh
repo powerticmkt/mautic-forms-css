7c2f1fbcf27654167f443cbfc7567fc2d344a759
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-10-18 19:33:49 -0200

    Ajustes

--- compile.sh
+++ compile.sh
@@ -5,3 +5,9 @@ sass scss/rd.scss rd.min.css --style compressed
 
 sass scss/happy.scss happy.css
 sass scss/happy.scss happy.min.css --style compressed
+
+sass scss/classic.scss classic.css
+sass scss/classic.scss classic.min.css --style compressed
+
+sass scss/orange.scss orange.css
+sass scss/orange.scss orange.min.css --style compressed
